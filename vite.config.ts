67d2becb48597f4e2b23fd700e0b3a597a8050a4
abhiram.r <abhiram.r@brainwired.in>
2025-03-22 17:06:58 +0530

    optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom"],
+          router: ["react-router-dom"],
+        },
+      },
+    },
+  },
+  server: {
+    hmr: true,
+    open: true,
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom", "react-router-dom"],
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
+});
